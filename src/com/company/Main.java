73de0f119160a227afc1e5d5b3bb41c90c41e073
iaroslav.sviatkin <iaroslav.sviatkin.globallogic.com>
2020-12-05 13:03:38 +0200

    commit4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,12 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
 
     }
 }
