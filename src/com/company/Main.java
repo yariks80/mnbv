e6c578419d607f05b0beece1563eeebf3da6d93f
iaroslav.sviatkin <iaroslav.sviatkin.globallogic.com>
2020-12-06 09:03:38 +0200

    TC1000

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,10 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
 
 
     }
